5befb86313db6785cfc980e960b9c673aae22b3c
Carchyj <janecarchy6@gmail.com>
2022-07-02 18:03:00 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - prints the alphabet in lowercase followed by a new line.
+*
+*Return: always 0(success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch = 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10); /* Ascii code for a new line*/
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,3 @@
 0.  program will assign a random number to the variable n each time it is executed.
 1. program will assign a random number to the variable n each time it is executed.
-
+2. program that will print the alphabets in lowercase, followed by a new line.
