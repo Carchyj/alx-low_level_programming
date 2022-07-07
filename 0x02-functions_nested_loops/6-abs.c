9d02cadc441f4cba430ea5a8a7aa0a22b3b00e02
Carchyj <janecarchy6@gmail.com>
2022-07-06 17:29:35 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+*_abs - computes absolute value of an integer
+*@y: parameter
+*Return: absolute vlue of a number.
+*/
+int _abs(int y)
+{
+	if (y < 0)
+		y = -y;
+	return (y);
+}
+
