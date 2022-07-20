dddaa9caef166899aebaecd4498c14743f3eb6e6
Carchyj <janecarchy6@gmail.com>
2022-07-19 18:28:00 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include  "main.h"
+
+/**
+ * _puts - function that puts.
+ * @str: parameter for thr put function.
+ */
+void _puts(char *str)
+{
+	for(; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
