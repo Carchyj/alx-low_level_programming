5d44f14722ea19a4b999661e9266c1570a0cbe76
Carchyj <janecarchy6@gmail.com>
2022-07-21 14:22:53 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string recursively.
+ * @s: parameter.
+ */
+void _puts_recursion(char *s)
+{
+	s = "ram\0"
+		if (*s == '\0')
+		{
+			_putchar('\n');
+			return;
+		}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
+
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIH_H
+int _putchar(char c);
+void _puts_recursion(char *s);
+#endif
